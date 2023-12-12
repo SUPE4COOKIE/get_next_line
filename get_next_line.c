caed4545c69bb2d4e858a296ec4b1c512aeda4a0
supe4cookie <supe4cookie@gmail.com>
2023-12-12 04:43:37 +0100

    wooo

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mwojtasi <mwojtasi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 08:19:44 by mwojtasi          #+#    #+#             */
-/*   Updated: 2023/12/11 05:44:10 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2023/12/12 04:17:11 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,34 @@
 t_list *get_next_line(int fd)
 {
 	static t_list *stash = NULL;
-	t_list *current;
 	size_t i;
+	char *buffer;
 
 	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while (i < BUFFER_SIZE)
+	buffer = read_buffer(fd);
+	while (!is_line(buffer))
 	{
-		current = append_buffer(stash, read_buffer(fd));
-		printf("%s\n", current->str);
-		if (is_line(current->str))
-			return (stash);
-		i++;
+		append_buffer(&stash, buffer);
+		buffer = read_buffer(fd);
 	}
+	append_buffer(&stash, buffer);
 	return (stash);
 }
 
 char	*read_buffer(int fd)
 {
 	char	*buffer;
+	size_t	read_bytes;
 	
-	buffer = malloc(BUFFER_SIZE);
-	read(fd, buffer, BUFFER_SIZE);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (NULL);
+	buffer[BUFFER_SIZE] = '\0';
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (read_bytes == 0)
+		return (free(buffer), printf("eof") ,NULL);
 	return (buffer);
 }
 
@@ -56,31 +61,67 @@ int is_line(char *buffer)
     return (0);
 }
 
-t_list *append_buffer(t_list *stash, char *buffer)
+t_list *append_buffer(t_list **stash, char *buffer)
 {
 	t_list *new;
 	t_list *current;
-	
+
 	new = malloc(sizeof(t_list));
 	if (new == NULL)
 		return (NULL);
 	new->str = buffer;
 	new->next = NULL;
-	if (stash == NULL)
-		return (new);
-	current = stash;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
+	if (*stash == NULL)
+		*stash = new;
+	else
+	{
+		current = *stash;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
 	return (new);
 } //move to utils
+
+size_t str_list_len(t_list *list)
+{
+	size_t	count;
+	size_t	i;
+	t_list	*current;
+
+	count = 0;
+	i = 0;
+	current = list;
+	while (current != NULL)
+	{
+		if (current->next != NULL)
+			count += BUFFER_SIZE;
+		else
+		{
+			while (current->str[i])
+				if (current->str[i++] == '\n')
+					return (count + i);
+			count += i;
+		}
+		current = current->next;
+	}
+	return (count);
+}
+
+//char *strcat_list(t_list *res)
+//{
+//	char 
+//}
 
 int main(int argc, char **argv)
 {
     int fd;
-
+	t_list *res;
+	
     fd = open(argv[1], O_RDONLY);
-    get_next_line(fd);
+    res = get_next_line(fd);
+	printf("%zu", str_list_len(res));
+	//get_next_line(fd);
     close(fd);
     return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mwojtasi <mwojtasi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 08:19:50 by mwojtasi          #+#    #+#             */
-/*   Updated: 2023/12/11 05:07:42 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2023/12/12 04:24:26 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ typedef struct s_list
 	struct s_list *next;
 } t_list;
 t_list *get_next_line(int fd);
-t_list *append_buffer(t_list *stash, char *buffer);
+t_list *append_buffer(t_list **stash, char *buffer);
 char	*read_buffer(int fd);
-void display_list(t_list *head);
+size_t	str_list_len(t_list *list);
 int is_line(char *buffer);
 #endif
